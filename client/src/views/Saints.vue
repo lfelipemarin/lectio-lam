06bb8bfdc68370b2e2bc762f23dffcc463524b42
Felipe Marin <felipe.marin@gmail.com>
2019-10-02 10:57:26 -0500

    Implemented saints list with avatars

--- client/src/views/Saints.vue
+++ client/src/views/Saints.vue
@@ -13,43 +13,52 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col v-for="(saint, index) in saints.data" :key="index" cols="12" sm="12" md="6" lg="4">
-        <v-card>
-          <v-img class="white--text" gradient="0deg, rgba(5,8,13,0.5508578431372548) 100%, rgba(0,212,255,1) 100%" height="200px" :src="saint.image_links.large" v-if="saint.image_links">
-            <v-card-title class="align-end fill-height">{{saint.name}}</v-card-title>
-          </v-img>
-          <!-- <v-chip class="mt-2" color="primary" label text-color="white">
-              <v-icon left>mdi-calendar-month</v-icon>
-              {{beautyDate(lectio.createdAt)}}
-            </v-chip> -->
-          <v-card-text>
-            <p>{{saint.name}}</p>
-          </v-card-text>
-          <v-card-actions>
-            <!-- <v-btn class="white--text" color="amber accent-4" @click="sendSelectionToLectio">
-              Lectio Divina</v-btn> -->
-            <div class="flex-grow-1"></div>
+      <v-col cols="12" sm="12" md="6" order-sm="2" order-md="1">
+        <v-list width="100%" subheader>
+          <v-subheader>Santos del día</v-subheader>
 
-            <v-btn icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
+          <v-list-item v-for="(saint, index) in saints.data" :key="index" @click="">
+            <v-list-item-avatar>
+              <v-img :src="showSaintAvatar(saint.image_links)"></v-img>
+            </v-list-item-avatar>
 
-            <v-btn icon>
-              <v-icon>mdi-bookmark</v-icon>
-            </v-btn>
+            <v-list-item-content>
+              <v-list-item-title v-text="saint.name"></v-list-item-title>
+            </v-list-item-content>
 
-            <v-btn icon>
-              <v-icon>mdi-share-variant</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+            <!-- <v-list-item-icon>
+              <v-icon :color="item.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
+            </v-list-item-icon> -->
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
         <v-snackbar v-model="snackbar" multi-line color="info" :timeout=4000>
           Por favor selecciona una parte del texto
           <v-btn color="white" text @click="snackbar = false">
             Cerrar
           </v-btn>
         </v-snackbar>
+      </v-col>
+      <v-col cols="12" sm="12" md="6" order-sm="1" order-md="2">
+        <p>¿Quienes son los santos? Son esa multitud innumerable de hombres y mujeres, de toda raza, edad y condición,
+          que se desvivieron por los demás, que vencieron el egoísmo, que perdonaron siempre. Santos son los que han
+          hecho de su vida una epifanía de los valores trascendentes; por eso quienes buscan a Dios lo encuentran con
+          facilidad humanizado en los santos.</p>
 
+        <p>
+          Me parece que es Bernanos el que ha escrito lo siguiente: "He perdido la infancia y no la puedo reconquistar
+          sino por medio de la santidad". ¿Qué es, pues, la santidad? La santidad es la totalidad del espíritu de las
+          Bienaventuranzas, que se leen en el evangelio de la Misa. La totalidad es pobreza, mansedumbre, justicia,
+          pureza, paz, misericordia. Es apertura y donación que tienen como símbolo la confianza de un niño.
+        </p>
+        <p>
+          Santidad es tener conciencia efectiva de ser hijo de Dios. Este sentido de filiación debe ser acrecentado a
+          través de la purificación interior y así alcanzar la meta plena de nuestra conformación con Dios. Santidad es
+          pluralidad. Cada uno debe seguir a Cristo desde su propia circunstancia y talante; desde su nación, raza y
+          lengua, en los días felices y cuando la tribulación arranca lágrimas del corazón; en la soledad del claustro o
+          en el vértigo de la ciudad; en la buena y en la mala salud.
+        </p>
+        <p>Fuente: https://es.catholic.net/op/articulos/67197/cat/1069/quienes-son-los-santos.html</p>
       </v-col>
     </v-row>
   </v-container>
@@ -82,7 +91,6 @@ export default {
     };
   },
   computed: {
-
   },
   watch: {
 
@@ -93,6 +101,10 @@ export default {
       let date = moment().format()
       const response = await lectioService.getSaintsByDate(date);
       this.saints = response.data;
+    },
+
+    showSaintAvatar (imageLinks) {
+      return (imageLinks && imageLinks.ico) ? imageLinks.ico : require("@/assets/heart-circle-outline.svg")
     },
 
     cleanText (text) {
