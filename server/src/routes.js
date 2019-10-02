58e93716f853b68286e2da9ea295b9a57bcba5cd
Felipe Marin <felipe.marin@gmail.com>
2019-10-02 14:23:22 -0500

    Minor change

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,7 +2,7 @@ const AuthenticationController = require('./controllers/AuthenticationController
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
 // const SongsController = require('./controllers/SongsController')
 const BookmarksController = require('./controllers/BookmarksController')
-const HistoriesController = require('./controllers/HistoriesController')
+// const HistoriesController = require('./controllers/HistoriesController')
 const LectioController = require('./controllers/LectioController')
 
 const isAuthenticated = require('./policies/isAuthenticated')
